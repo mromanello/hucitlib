b69b34a5f1c1a14991974b289880e8fec1a13ca1
mromanello <matteo.romanello@gmail.com>
2017-03-09 17:35:50 +0100

    changed travis config file (install with -e flag) so that coveralls work when running py.tests

--- tests/test_kb.py
+++ tests/test_kb.py
@@ -2,24 +2,13 @@
 # author: Matteo Romanello, matteo.romanello@gmail.com
 import logging
 import pytest
-from knowledge_base import KnowledgeBase
-from pytest import fixture
 import pdb
 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-@fixture
-def kb_inmemory(configuration_file="inmemory.ini"):
-	if(configuration_file is not None):
-		return KnowledgeBase(configuration_file)
-@fixture
-@pytest.mark.skip(reason="need to deal with installing virtuoso at build-time")
-def kb_virtuoso(configuration_file="virtuoso.ini"):
-	if(configuration_file is not None):
-		return KnowledgeBase(configuration_file)
-@pytest.mark.skip(reason="need to deal with installing virtuoso at build-time")
+#@pytest.mark.skip(reason="need to deal with installing virtuoso at build-time")
 def test_kb_virtuoso(kb_virtuoso):
 	logger.debug(kb_virtuoso)
 	authors = kb_virtuoso.get_authors()
