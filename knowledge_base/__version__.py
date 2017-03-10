0e1cb27847d7bb01b2e5f0e5b447b01ce456c709
mromanello <matteo.romanello@gmail.com>
2017-03-10 16:27:08 +0100

    bumping to v. 0.2.1

--- knowledge_base/__version__.py
+++ knowledge_base/__version__.py
@@ -5,5 +5,5 @@
 
 from collections import namedtuple
 VersionSpec = namedtuple('VersionSpec', ['major', 'minor', 'revision'])
-version = VersionSpec(0, 1, 1)
+version = VersionSpec(0, 2, 1)
 str_version = '.'.join(map(str, version))
