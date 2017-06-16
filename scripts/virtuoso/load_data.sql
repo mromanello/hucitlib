ae0694aafa617db4cd7dc73b4365c1951c282389
mromanello <matteo.romanello@gmail.com>
2017-06-16 12:51:08 +0200

    added some bash scripts to load/dump/clear the KB data from virtuoso

--- scripts/virtuoso/load_data.sql
+++ scripts/virtuoso/load_data.sql
@@ -6,9 +6,9 @@ delete from DB.DBA.load_list;
 
 -- see http://www.openlinksw.com/dataspace/dav/wiki/Main/VirtBulkRDFLoader
 select 'Loading data...';
---      <folder with data>  <pattern>    <default graph if no graph file specified>
-DB.DBA.TTLP_MT (http_get('https://raw.githubusercontent.com/mromanello/hucit_kb/master/knowledge_base/data/kb/kb-all-in-one.ttl'), '', 'http://purl.org/hucit/kb');
---ld_dir('/Users/rromanello/Documents/ClassicsCitations/hucit_kb/knowledge_base/data/kb/','kb-all-in-one.ttl','http://purl.org/hucit/kb');
+-- load data directly from main branch of the github repo
+DB.DBA.TTLP_MT (http_get('https://raw.githubusercontent.com/mromanello/hucit_kb/master/knowledge_base/data/kb/hucit_000001.ttl'), '', 'http://purl.org/hucit/kb');
+
 
 rdf_loader_run();
 
