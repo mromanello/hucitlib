7be893408c299f88d6774d514259f927925dd799
Matteo Romanello <matteo.romanello@gmail.com>
2019-03-10 10:12:50 +0100

    bumped to v. 0.3.0

--- knowledge_base/__version__.py
+++ knowledge_base/__version__.py
@@ -5,5 +5,5 @@
 
 from collections import namedtuple
 VersionSpec = namedtuple('VersionSpec', ['major', 'minor', 'revision'])
-version = VersionSpec(0, 2, 9)
+version = VersionSpec(0, 3, 0)
 str_version = '.'.join(map(str, version))
