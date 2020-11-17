5db016a11c4ca1fa06646071119bc17d59ce5edd
Matteo Romanello <matteo.romanello@epfl.ch>
2020-11-17 15:43:08 +0100

    updated deps in setup.py + requirements.txt

--- setup.py
+++ setup.py
@@ -1,39 +1,40 @@
 import os
 from setuptools import setup, find_packages
 
-NAME = "knowledge_base"
-exec(open('{0}/__version__.py'.format(NAME)).read())
+NAME = "hucitlib"
+exec(open("{0}/__version__.py".format(NAME)).read())
 VERSION = str_version
 
-# TODO: update for python3
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name='hucitlib'
-	, author='Matteo Romanello'
-	, author_email='matteo.romanello@gmail.com'
-	, url='https://github.com/mromanello/hucit_kb/'
-    , version=VERSION
-    , packages=find_packages()
-    , package_data={'knowledge_base': [
-                                    'data/*.*'
-                                    , 'data/kb/*.*'
-                                    , 'config/*.*'
-                                    ]}
-    , entry_points={
-        'console_scripts':[
-            'hucit = knowledge_base.cli:main'
-        ]
-    }
-    , classifiers=[
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3.6',
-        'Operating System :: POSIX'
-    ]
-    , license='GPL v3'
+
+setup(
+    name="hucitlib",
+    author="Matteo Romanello",
+    author_email="matteo.romanello@gmail.com",
+    url="https://github.com/mromanello/hucit_kb/",
+    version=VERSION,
+    packages=find_packages(),
+    package_data={"hucitlib": ["data/*.*", "data/kb/*.*", "config/*.*"]},
+    entry_points={"console_scripts": ["hucit = hucitlib.cli:main"]},
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: POSIX",
+    ],
+    license="GPL v3"
     # TODO: find a way to install Thibault's fork of surf from here
     # so that it works when installing by pip.
-    , install_requires=['pyCTS', 'surf>=1.2.0', 'docopt']
-    , long_description=read('README.md')
+    ,
+    install_requires=[
+        "pyCTS",
+        "surf>=1.2.0",
+        "docopt",
+        "tqdm",
+        "retrying",
+        "rdflib>=4.2.1",
+    ],
+    long_description=read("README.md"),
 )
